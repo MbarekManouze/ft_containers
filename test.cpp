14baeb3227fe06346769ce825499974fa58458dc
M'barek Manouze <mmanouze@e1r2p11.1337.ma>
2023-01-21 18:08:56 +0100

    working on member functions of the vector

--- test.cpp
+++ test.cpp
@@ -12,11 +12,11 @@ void ft_resize()
   for (int i=1;i<10;i++) 
     myvector.push_back(i);  
   std::cout << "capacity : "<<myvector.capacity() << std::endl;
-  myvector.resize(8,100);
-  std::cout << "capacity  "<<myvector.capacity() << std::endl;
-  myvector.resize(5);
-  std::cout << "capacity  "<<myvector.capacity() << std::endl;
   myvector.resize(12);
+  std::cout << "capacity  "<<myvector.capacity() << std::endl;
+  myvector.resize(20,100);
+  myvector.resize(11);
+  std::cout << "capacity  "<<myvector.capacity() << std::endl;
   std::cout << "capacity  "<<myvector.capacity() << std::endl;
   std::cout << "size  "<<myvector.size() << std::endl;
 
@@ -33,11 +33,12 @@ void ft_resize()
   for(int i=1; i<10;i++)
     vec.push_back(i);
   std::cout << "capacity : "<<vec.capacity() << std::endl;
-  vec.resize(8,100);
-  std::cout << "capacity : "<<vec.capacity() << std::endl;
-  vec.resize(5);
-  std::cout << "capacity : "<<vec.capacity() << std::endl;
   vec.resize(12);
+  std::cout << "capacity : "<<vec.capacity() << std::endl;
+  vec.resize(20,100);
+  vec.resize(11);
+  std::cout << "capacity : "<<vec.capacity() << std::endl;
+  //std::cout << vec.size() << std::endl;
   std::cout << "capacity : "<<vec.capacity() << std::endl;
   std::cout << "size : "<<vec.size() << std::endl;
 
@@ -45,12 +46,132 @@ void ft_resize()
   for (int i=0;i<vec.size();i++)
     std::cout << ' ' << vec[i];
   std::cout << '\n';
+}
+
+void ft_erase()
+{
+  std::vector<int> myvector;
+
+  // set some values (from 1 to 10)
+  for (int i=1; i<=10; i++) myvector.push_back(i);
+
+  // erase the 6th element
+  std::cout << "capacity : "<< myvector.capacity() << std::endl;
+  std::cout << "size     : "<<myvector.size() << std::endl;
+  myvector.erase (myvector.begin()+5);
+  //myvector.erase (myvector.begin());
+  //myvector.erase (myvector.end());
+  std::cout << "capacity : "<< myvector.capacity() << std::endl;
+  std::cout << "size     : "<<myvector.size() << std::endl;
+
+  // erase the first 3 elements:
+  myvector.erase (myvector.begin(),myvector.begin()+3);
+  std::cout << "capacity : "<< myvector.capacity() << std::endl;
+  std::cout << "size     : "<<myvector.size() << std::endl;
+
+  std::cout << "myvector contains:";
+  for (unsigned i=0; i<myvector.size(); ++i)
+    std::cout << ' ' << myvector[i];
+  std::cout << '\n';
+
+
+  std::cout << "************************************\n";
+  
+  vector<int> vec;
+
+  for (int i=1; i<=10; i++) vec.push_back(i);
+
+  std::cout << "capacity : "<< vec.capacity() << std::endl;
+  std::cout << "size     : "<<vec.size() << std::endl;
+  vec.erase (vec.begin()+5);
+  //vec.erase (vec.begin());
+  //vec.erase (vec.end());
+  std::cout << "capacity : "<< vec.capacity() << std::endl;
+  std::cout << "size     : "<<vec.size() << std::endl;
+
+  vec.erase(vec.begin(), vec.begin()+3);
+  std::cout << "capacity : "<<vec.capacity()<<std::endl;
+  std::cout << "size     : "<<vec.size()<<std::endl;
+
+  std::cout << " vec contains    :";
+  for (unsigned i=0; i< vec.size(); ++i)
+    std::cout << ' ' <<  vec[i];
+  std::cout << '\n';
+
+}
+
+void ft_get_allocator()
+{
+  std::vector<int> myvector;
+  int * p;
+  unsigned int i;
+
+  // allocate an array with space for 5 elements using vector's allocator:
+  p = myvector.get_allocator().allocate(5);
+
+  // construct values in-place on the array:
+  for (i=0; i<5; i++) myvector.get_allocator().construct(&p[i],i);
+
+  std::cout << "The allocated array contains:";
+  for (i=0; i<5; i++) std::cout << ' ' << p[i];
+  std::cout << '\n';
+
+  // destroy and deallocate:
+  for (i=0; i<5; i++) myvector.get_allocator().destroy(&p[i]);
+  myvector.get_allocator().deallocate(p,5);
+
+  std::cout << "***********************" << std::endl;
+
+  vector<int> vec;
+  int * pointer;
+  unsigned int l;
+
+
+
+  pointer = vec.get_allocator().allocate(5);
+
+
+  for(l = 0; i < 5; i++)
+    vec.get_allocator().construct(&pointer[i], i);
+
+  std::cout << "The allocated array contains:";
+  for (i=0; i<5; i++)
+    std::cout << ' ' << pointer[i];
+  std::cout << '\n';
+
+  for (i=0; i<5; i++)
+    myvector.get_allocator().destroy(&pointer[i]);
+  myvector.get_allocator().deallocate(pointer,5);
+
 
 }
 
 int main ()
 {
-  ft_resize();
+  //ft_resize();
+  //ft_erase();
+  //ft_get_allocator();
+  std::vector<int> myvector(4);
+  std::cout << "capacity : "<< myvector.capacity() << std::endl;
+  std::cout << "size     : "<< myvector.size() << std::endl;
+  myvector.insert(myvector.begin(), 200);
+  std::cout << "capacity : "<< myvector.capacity() << std::endl;
+  std::cout << "size : "<< myvector.size()<<std::endl;
+  for (size_t i = 0; i < myvector.size(); i++)
+    std::cout << myvector[i] << std::endl;
+
+  std::cout << "***************************\n";
+
+  vector<int> vec(4);
+  std::cout << "capacity : "<< vec.capacity() << std::endl;
+  std::cout << "size     : "<< vec.size() << std::endl;
+  vec.insert(vec.begin(), 200);
+  std::cout << "capacity : "<< vec.capacity() << std::endl;
+  std::cout << "size : "<< vec.size()<<std::endl;
+  for (size_t i = 0; i < vec.size(); i++)
+    std::cout << vec[i] << std::endl;
+
+
   //system("leaks a.out");
   return 0;
 }
--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: mmanouze <mmanouze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 15:52:11 by mmanouze          #+#    #+#             */
-/*   Updated: 2023/01/19 19:34:30 by mmanouze         ###   ########.fr       */
+/*   Updated: 2023/01/21 18:03:53 by mmanouze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,22 +36,30 @@ template < class T, class Alloc = std::allocator<T> > class vector
 		
 		vector():_size(0), _capacity(0), allocater(), _data(0){}
 		vector(size_t n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
-		: allocater(alloc),_capacity(n), _size(0){
+		: allocater(alloc),_capacity(n), _size(n){
 			_data = allocater.allocate(n);
+			for (size_t i = 0; i < n; i++)
+				allocater.construct(_data + i, val);
 		}
 		~vector(){ allocater.deallocate(_data, _capacity); }
 
 		value_type &operator[](size_t index){ return (_data[index]); }
 
-		iterator 		begin() { return (iterator (_data)); }
-		iterator 		end() { return (iterator (_data + _size)); }
-		const_Iterator 	cbegin() const throw() { return (iterator (_data)); }
-		const_Iterator 	cend() const throw() { return (iterator (_data + _size)); }
-		size_type 		size() const { return (_size); }
-		size_t 			max_size() const { return std::numeric_limits<std::size_t>::max() / sizeof(T); }
-		size_type 		capacity() const { return (_capacity); }
-		bool 			empty() const { return (_size == 0 ? true : false); }
-		
+		iterator 				begin() { return (iterator (_data)); }
+		iterator 				end() { return (iterator (_data + _size)); }
+		const_Iterator 			cbegin() const throw() { return (iterator (_data)); }
+		const_Iterator 			cend() const throw() { return (iterator (_data + _size)); }
+		size_type 				size() const { return (_size); }
+		size_t 					max_size() const { return std::numeric_limits<std::size_t>::max() / sizeof(T); }
+		size_type 				capacity() const { return (_capacity); }
+		bool 					empty() const { return (_size == 0 ? true : false); }
+		reference     			at(size_type n) { if (n > 0 && n < _capacity) { return (_data[n]); } }
+		const_reference     	at(size_type n) const { if (n > 0 && n < _capacity) { return (_data[n]); } }
+		reference 				back() { return (_data[_size - 1]); }
+		const_reference 		back() const { return (_data[_size - 1]); }
+		reference 				front() { return (_data[0]); }
+		const_reference 		front() const { return (_data[0]); }
+		allocator_type 			get_allocator() const { return (allocater); }
 
 		// RESIZE METHOD DONE AND WELL TESTED //
 
@@ -75,7 +83,7 @@ template < class T, class Alloc = std::allocator<T> > class vector
 							new_capacity = _capacity * 2;
 					}
 					else
-						new_capacity = n;
+						new_capacity = _capacity;
 					pointer _new_data = allocater.allocate(new_capacity);
 					for (size_t i = 0; i < this->size() ; i++)
 						allocater.construct(_new_data + i, _data[i]);
@@ -103,13 +111,16 @@ template < class T, class Alloc = std::allocator<T> > class vector
 			}
 		}
 
-		void resize (size_type n, const T& value){
+		void 			resize (size_type n, const T& value){
 			if (n > this->size()){
 				size_t new_capacity;
-				if (n > (_capacity * 2))
-					new_capacity = n;
 				if (n > _capacity)
-					new_capacity = _capacity * 2;
+				{
+					if (n > _capacity * 2)
+						new_capacity = n;
+					else
+						new_capacity = _capacity * 2;
+				}
 				else
 					new_capacity = _capacity;
 				pointer new_data = allocater.allocate(new_capacity);
@@ -142,16 +153,16 @@ template < class T, class Alloc = std::allocator<T> > class vector
 
 			// RESIZE FUNCTION IMPLEMENTATION
 
+			if (_size == 0)
+				_capacity *= 2;
 			if (_size == _capacity)
 			{
-				//std::cout << "capacity     :  " << _capacity << std::endl;
 				size_t new_capacity = _capacity ? _capacity * 2 : 1;
-				//std::cout << "new_capacity :  " << new_capacity << std::endl;
 				pointer _new_data = allocater.allocate(new_capacity);
 				for (size_t i = 0; i < _size; i++)
 					allocater.construct(_new_data + i , _data[i]);
-				//for (size_t i = _size ; i < new_capacity ; i++)
-				//	allocater.construct(_new_data + i, 0);
+				for (size_t i = _size ; i < new_capacity ; i++)
+					allocater.construct(_new_data + i, 0);
 				for (size_t i = 0; i < _size; i++)
 					allocater.destroy(_data + i);
 				allocater.deallocate(_data ,_size);
@@ -166,6 +177,72 @@ template < class T, class Alloc = std::allocator<T> > class vector
 			//std::cout << "alright "<< std::endl;
 		}
 
+		/////////////////////////////////
+
+		iterator erase (iterator position){
+			for (size_t i = 0; i < _size; i++){
+				if (_data[i] == *position){
+					size_t t = i + 1;
+					while (i < _size && t <= _size){
+						_data[i] = _data[t];
+						i++;
+						t++;
+					}
+				}
+			}
+			_size--;
+			return (position);
+		}
+
+		iterator erase (iterator first, iterator last){
+			size_t diffrence;
+			for (size_t i = 0; i < _size ;i++){
+				if (*first == _data[i]){
+					size_t t = i;
+					while (_data[t] != *last)
+						t++;
+					diffrence = t - i;
+					while (i < _size && t <= _size){
+						_data[i] = _data[t];
+						i++;
+						t++;
+					}
+				}
+			}
+			_size -= diffrence;
+			return (first);
+		}
+
+		////////////////////////////////
+
+
+		iterator insert (iterator position, const value_type& val){
+			size_t 	tmp;
+			pointer new_data;
+			if (_size == _capacity)
+				_capacity *= 2;
+			new_data = allocater.allocate(_capacity);
+			_size++;
+			for (size_t i = 0; i < _size ; i++){
+				if (*position == _data[i]){
+					tmp = i;
+					allocater.construct(new_data + i, val);
+					i++;
+					while (tmp < _size){
+						allocater.construct(new_data + i, _data[tmp]);
+						tmp++;
+						i++;
+					}
+					for (size_t i = 0; i < _capacity; i++)
+						allocater.destroy(_data + i);
+					allocater.deallocate(_data, _capacity);
+					_data = new_data;
+					return (position);
+				}
+				allocater.construct(new_data + i, _data[i]);
+			}
+			return (position);
+		}
 
 	private :
 		T				*_data;
